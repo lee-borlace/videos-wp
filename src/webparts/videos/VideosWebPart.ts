753c9839c1d564269e15115eacfa03618404f619
leeroy79 <lee.borlace@gmail.com>
2017-05-01 20:15:45 +0800

    Copy SPFx files across.

---
+++ src/webparts/videos/VideosWebPart.ts
@@ -0,0 +1,53 @@
+import * as React from 'react';
+import * as ReactDom from 'react-dom';
+import { Version } from '@microsoft/sp-core-library';
+import {
+  BaseClientSideWebPart,
+  IPropertyPaneConfiguration,
+  PropertyPaneTextField
+} from '@microsoft/sp-webpart-base';
+
+import * as strings from 'videosStrings';
+import Videos from './components/Videos';
+import { IVideosProps } from './components/IVideosProps';
+import { IVideosWebPartProps } from './IVideosWebPartProps';
+
+export default class VideosWebPart extends BaseClientSideWebPart<IVideosWebPartProps> {
+
+  public render(): void {
+    const element: React.ReactElement<IVideosProps > = React.createElement(
+      Videos,
+      {
+        description: this.properties.description
+      }
+    );
+
+    ReactDom.render(element, this.domElement);
+  }
+
+  protected get dataVersion(): Version {
+    return Version.parse('1.0');
+  }
+
+  protected getPropertyPaneConfiguration(): IPropertyPaneConfiguration {
+    return {
+      pages: [
+        {
+          header: {
+            description: strings.PropertyPaneDescription
+          },
+          groups: [
+            {
+              groupName: strings.BasicGroupName,
+              groupFields: [
+                PropertyPaneTextField('description', {
+                  label: strings.DescriptionFieldLabel
+                })
+              ]
+            }
+          ]
+        }
+      ]
+    };
+  }
+}
---
+++ src/webparts/videos/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface IVideosStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'videosStrings' {
+  const strings: IVideosStrings;
+  export = strings;
+}
