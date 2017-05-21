95ee9a6fe0414b48d3c3246859c36544eaba03e0
leeroy79 <lee.borlace@gmail.com>
2017-05-21 09:04:21 +0800

    Tempoararily include context in properties of top-level component.

--- src/webparts/videos/components/Videos.tsx
+++ src/webparts/videos/components/Videos.tsx
@@ -11,6 +11,7 @@ export default class Videos extends React.Component<IVideosProps, void> {
           <div className={`ms-Grid-row ms-bgColor-themeDark ms-fontColor-white ${styles.row}`}>
             <div className="ms-Grid-col ms-u-lg10 ms-u-xl8 ms-u-xlPush2 ms-u-lgPush1">
               <span className="ms-font-xl ms-fontColor-white">Welcome to SharePoint!</span>
+              <p className="ms-font-l ms-fontColor-white">Web Title : {escape(this.props.webPartContext.pageContext.web.title)}</p>
               <p className="ms-font-l ms-fontColor-white">videosServerRelativeUrl : {escape(this.props.videosServerRelativeUrl)}</p>
               <p className="ms-font-l ms-fontColor-white">focusedVideoKeyword : {escape(this.props.focusedVideoKeyword)}</p>
               <p className="ms-font-l ms-fontColor-white">primaryVideoKeyword : {escape(this.props.primaryVideoKeyword)}</p>
